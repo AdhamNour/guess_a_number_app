a98e9e2c0faadd66f96bbbfaddcecac5de486528
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-03-22 10:20:19 +0200

    finished video 9 from module 4

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,48 +1,54 @@
-import React from 'react';
-import { View,Text,StyleSheet,TextInput,Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
-import Card from '../components/Card'
+import Card from "../components/Card";
+import Colors from "../constants/colors"
 
 const StartGameScreen = () => {
-    return ( 
-        <View style={styles.screen} >
-            <Text style={styles.title} >The Game Screen</Text>
-            <Card style={styles.inputContainer} >
-                <Text>
-                    Select A number
-                </Text>
-                <TextInput />
-                <View style={styles.buttonContainer} >
-                    <Button title="Reset" onPress={() =>{}}/>
-                    <Button title="Confirm" onPress={() =>{}}/>
-                </View>
-            </Card>
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>The Game Screen</Text>
+      <Card style={styles.inputContainer}>
+        <Text>Select A number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color= {Colors.accentColor}/>
+          </View>
+          <View style={styles.button}>
+            <Button title="Confirm" onPress={() => {}} color={Colors.primaryColor} />
+          </View>
         </View>
-     );
-}
+      </Card>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex:1,
-        padding:10,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-    title: {
-        fontSize:20,
-        marginVertical:10
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        width:'100%',
-        justifyContent:'space-between',
-        paddingHorizontal:15
-    },
-    inputContainer: {
-        width:300,
-        maxWidth:'80%',
-        alignItems: 'center',
-    }
-})
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  button: {
+      width:100
+  }
+});
 
 export default StartGameScreen;
