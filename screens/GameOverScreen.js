ef8e84b7bb0be069a97a985d73c8f6b3948261db
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-04-03 22:43:53 +0200

    minor changes

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { View,Text,StyleSheet,Button } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const GameOverScreen = (props) => {
-    return ( 
-        <View>
-            <Text>this is game over screen, Guessed in {props.roundCount} rounds</Text> 
-            <Button title="Reset" onPress={props.reset}  />
-        </View>
-     );
-}
- 
+  return (
+    <View style={styles.screen}>
+      <Text>this is game over screen Guessed in</Text>
+      <Text> Guessed in {props.roundCount} rounds</Text>
+      <Button title="Reset" onPress={props.reset} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
 export default GameOverScreen;
