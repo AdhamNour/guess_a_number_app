ea693a564a7227c5e4abdbc9ce3e5742052ea367
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-04-15 00:54:19 +0200

    finished video 26 from module 4

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+import BodyText from "../components/BodyText"
+import TitleText from "../components/TitleText"
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
+      <TitleText>Game is Over</TitleText>
+      <Image source={require('../assets/images/success.png')} />
       <Text>this is game over screen Guessed in</Text>
       <Text> Guessed in {props.roundCount} rounds</Text>
       <Button title="Reset" onPress={props.reset} />
